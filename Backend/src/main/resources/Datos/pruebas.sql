83336327e15636499bc0a78e283b8930140cd747
Franciscoxd1123 <francisco.cea@usach.cl>
2024-12-15 22:22:57 -0300

    Cambio menor

--- Backend/src/main/resources/Datos/pruebas.sql
+++ Backend/src/main/resources/Datos/pruebas.sql
@@ -1,16 +1,16 @@
-SELECT * FROM almacen ORDER BY id_almacen;
+SELECT * FROM almacen ORDER BY id_almacen ASC;
 
-SELECT * FROM almacen_producto ORDER BY id_almacen_producto;
+SELECT * FROM almacen_producto ORDER BY id_almacen_producto ASC;
 
-SELECT * FROM categoria ORDER BY id_categoria;
+SELECT * FROM categoria ORDER BY id_categoria ASC;
 
-SELECT * FROM cliente ORDER BY id_cliente;
+SELECT * FROM cliente ORDER BY id_cliente ASC;
 
-SELECT * FROM detalle_orden ORDER BY id_detalle;
+SELECT * FROM detalle_orden ORDER BY id_detalle ASC;
 
-SELECT * FROM log_auditoria ORDER BY id_log;
+SELECT * FROM log_auditoria ORDER BY id_log ASC;
 
-SELECT * FROM orden ORDER BY id_orden;
+SELECT * FROM orden ORDER BY id_orden ASC;
 
 SELECT * FROM producto ORDER BY id_producto ASC;
 
